a2cdeb2ddefc32ee5621e75e95ffca768ba609e3
unknown <prashant.sharma@tecdune.com>
2024-05-17 15:00:28 +0530

    Create Utils file

--- Readme.md
+++ Readme.md
@@ -1,5 +1,11 @@
 #start with backened
 
 #here we provide the list of packages and library which is used in this project
-1- renderer
+1- renderer - cloud hoisting application
 2- cloudinary
+3- prettier - code pattern 
+4- nodemon - watch the changes and built automatically
+5- mongodb (
+    1- mongodb atlas - sub service of mongodb where they provide online db
+)
+6- mongoose- used to connnect db
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,21 @@
+const asyncHandler = ( reqHandler ) => {
+    ( req, res, next ) => {
+        Promise.resolve( reqHandler( req, res, next ) )
+        .catch( ( err ) => next( err ) )
+    }
+}
+
+export { asyncHandler }
+
+/*  we can write also **/
+
+// const asyncHandler = ( fn ) => async ( req, res, next ) => {
+//     try {
+//         await fn( req, res, next )
+//     } catch ( err ) {
+//         res.satus( err.code || 500 ).json({
+//             success: truwe,
+//             message: err.message
+//         })
+//     }
+// }
